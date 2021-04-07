04b53fdee1c5dc2e01b9a9f5ac856627f5000ebe
Dimple13 <59231474+Dimple13@users.noreply.github.com>
2021-04-07 23:14:21 +0530

    First page done

--- face-detector/src/App.js
+++ face-detector/src/App.js
@@ -1,14 +1,32 @@
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
+import Rank from './components/rank/Rank';
+import Imagelinkform from './components/imagelinkform/Imagelinkform';
 import './App.css';
-
+import Particles from 'react-particles-js';
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 70,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+      params={particlesOptions}
+      />
       <Navigation/>
       <Logo/>
+      <Rank/>
+      <Imagelinkform/>
       {/* {
-      <ImageLinkForm/>
+      
       <FaceRecognition/>} */}
     </div>
   );
